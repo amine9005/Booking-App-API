6a414369e2c2401afa765f5e6809b37cb892c976
amine9005 <amamselmitrader@gmail.com>
2024-04-21 23:35:09 +0100

    mongoDB connection

--- config/config.ts
+++ config/config.ts
@@ -3,9 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const SERVER_PORT = process.env.SERVER_PORT || "5500";
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017";
 
 export const config = {
   server: {
     port: SERVER_PORT,
+  },
+  mongo: {
+    url: MONGO_URL,
   },
 };
