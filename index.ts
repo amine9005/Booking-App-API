5a9e0f389ea8525d2ac3c33a9870b99961612d22
amine9005 <amamselmitrader@gmail.com>
2024-04-23 13:08:47 +0100

    updates

--- index.ts
+++ index.ts
@@ -1,9 +1,20 @@
 import createServer from "./app";
+import mongoose from "mongoose";
 import { config } from "./config/config";
 
 const app = createServer();
 const PORT = config.server.port;
 
-app.listen(PORT, (): void => {
-  console.log(`Server Running  at 👉 http://localhost:${PORT}`);
-});
+mongoose.Promise = Promise;
+mongoose
+  .connect(config.mongo.url, { retryWrites: true, w: "majority" }) //, {retryWrites:true,w:'majority'}
+  .then(() => {
+    console.log("Connected to Mongo DB successfully");
+    app.listen(PORT, (): void => {
+      console.log(`Server Running  at 👉 http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Couldn't connect to Mongo DB: ", err);
+    process.exit(1);
+  });
