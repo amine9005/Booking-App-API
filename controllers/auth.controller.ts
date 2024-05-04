5da67a082bb5ffc3ebc9f18221b8f687aff3bb22
amine9005 <amamselmitrader@gmail.com>
2024-05-04 23:43:18 +0100

    auth update

---
+++ controllers/auth.controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+
+const login = async (req: Request, res: Response, next: NextFunction) => {
+  return res.status(200).json({ message: "success" });
+};
+
+export default { login };
