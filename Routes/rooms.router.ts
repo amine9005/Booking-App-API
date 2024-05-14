768856e542b9dedaf80b3b111b302dfbfccb6839
amine9005 <amamselmitrader@gmail.com>
2024-05-14 22:29:24 +0100

    update

--- Routes/rooms.router.ts
+++ Routes/rooms.router.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
+import roomsController from "../controllers/rooms.controller";
 
 const router = express.Router();
 
 router.post("/", (req: Request, res: Response) => {
   res.status(200).json("Add Room Endpoint");
 });
+
+router.get("/bedsNumber", roomsController.getNumberOfBeds);
 
 export default router;
---
+++ controllers/rooms.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+
+const getNumberOfBeds = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  return res.status(200).json({ message: "success" });
+};
+
+export default { getNumberOfBeds };
