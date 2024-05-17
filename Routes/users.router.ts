849e0f071653a0def1e2c5e6bbe6cb42ffbda97d
amine9005 <amamselmitrader@gmail.com>
2024-05-17 23:41:14 +0100

    update

--- Routes/users.router.ts
+++ Routes/users.router.ts
@@ -5,5 +5,8 @@ const router = express.Router();
 router.post("/", (req: Request, res: Response) => {
   res.status(200).json("users Endpoint");
 });
+router.post("/", (req: Request, res: Response) => {
+  res.status(200).json("users Endpoint");
+});
 
 export default router;
