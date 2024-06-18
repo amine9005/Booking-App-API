ee13314bcd0e9b13f6c1e864991fa14b1063d63f
amine9005 <amamselmitrader@gmail.com>
2024-06-18 23:28:08 +0100

    update

--- models/user.model.ts
+++ models/user.model.ts
@@ -13,6 +13,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 export default mongoose.model("User", UserSchema);
