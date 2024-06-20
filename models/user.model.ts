5ade2df0ab1319dfccda8073c3aec2e7ecdd6e5d
amine9005 <amamselmitrader@gmail.com>
2024-06-20 23:52:12 +0100

    update

--- models/user.model.ts
+++ models/user.model.ts
@@ -21,6 +21,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
 });
 
 export default mongoose.model("User", UserSchema);
