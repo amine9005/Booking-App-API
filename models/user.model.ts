0df191d3830ec6daa04c20c82ba8dec548ee1861
amine9005 <amamselmitrader@gmail.com>
2024-06-22 23:22:18 +0100

    update

--- models/user.model.ts
+++ models/user.model.ts
@@ -29,6 +29,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  zipCode: {
+    type: String,
+    required: true,
+  },
 });
 
 export default mongoose.model("User", UserSchema);
