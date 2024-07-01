1c564df3728475adc1069fab176b6554f96b313d
amine9005 <amamselmitrader@gmail.com>
2024-07-01 23:59:11 +0100

    update

--- models/room..model.ts
+++ models/room..model.ts
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
-const RoomSchema = new mongoose.Schema({});
+const RoomSchema = new mongoose.Schema({
+  phoneNumber2: {
+    type: Number,
+    required: true,
+  },
+});
 
 export default mongoose.model("Room", RoomSchema);
